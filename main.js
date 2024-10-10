2d46fb2efce1e4d5098a2255531aa4582fc409f7
Khris <thkhrisgian@gmail.com>
2024-10-09 23:45:44 -0500

    projects section added

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ import "./styles/variables.css";
 import "./styles/utils.css";
 import "./styles/components/navbar.css";
 import "./styles/components/hero.css";
+import "./styles/components/projects.css";
 import "./styles/components/mainContent.css";
 import "./components/navbar.js";
